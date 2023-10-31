add247494ac13e49df8a8187181c9ee53e18a0c2
Vic <victortagupa@gmail.com>
2023-11-01 01:37:56 +0800

    Rename vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
             </v-card>
         </v-main>
         <v-footer app name="footer" class="justify-end" color="transparent">
-            <Footer />
+            <SiteInfo />
         </v-footer>
     </v-layout>
 </template>
