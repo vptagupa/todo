4749ae4f0d2643661cf0a024e6d72da47c4e4ca4
Vic <victortagupa@gmail.com>
2023-10-31 17:03:22 +0800

    Add transitions

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,7 @@
                         </v-chip>
                     </v-btn>
                     <v-btn
+                        v-if="tasksDoneCount"
                         size="small"
                         variant="flat"
                         color="red-accent-4"
@@ -59,60 +60,71 @@
                 </v-toolbar>
 
                 <v-card-text>
-                    <v-list>
-                        <v-list-item
-                            class="todo-item rounded-lg ma-2"
-                            v-for="(task, index) in tasks"
-                        >
-                            <div class="d-flex justify-space-between">
-                                <div>
-                                    <div class="d-flex align-baseline">
-                                        <div class="mr-5">
+                    <div class="task-list">
+                        <v-list>
+                            <TransitionGroup name="list" tag="v-list-item">
+                                <v-list-item
+                                    class="todo-item rounded-lg ma-2"
+                                    :class="{ 'task-done': task.status.done }"
+                                    v-for="(task, index) in tasks"
+                                >
+                                    <div class="d-flex justify-space-between">
+                                        <div>
+                                            <div class="d-flex align-baseline">
+                                                <div class="mr-5">
+                                                    <v-btn
+                                                        size="sm"
+                                                        variant="flat"
+                                                        :class="{
+                                                            'bg-success':
+                                                                !task.status
+                                                                    .done,
+                                                            'bg-grey-lighten-2':
+                                                                task.status
+                                                                    .done,
+                                                        }"
+                                                        class="rounded-pill pa-1"
+                                                        @click="
+                                                            updateStatus(
+                                                                task.id
+                                                            )
+                                                        "
+                                                    >
+                                                        <v-icon
+                                                            icon="mdi-check"
+                                                            size="sm"
+                                                        ></v-icon>
+                                                    </v-btn>
+                                                </div>
+                                                <div>{{ task.name }}</div>
+                                            </div>
+                                        </div>
+
+                                        <div>
                                             <v-btn
                                                 size="sm"
                                                 variant="flat"
-                                                :class="{
-                                                    'bg-success':
-                                                        !task.status.done,
-                                                    'bg-grey-lighten-2':
-                                                        task.status.done,
-                                                }"
-                                                class="rounded-pill pa-1"
-                                                @click="updateStatus(task.id)"
+                                                color="red"
+                                                class="pa-1 list-item-delete"
+                                                @click="deleteTodo(task.id)"
                                             >
                                                 <v-icon
-                                                    icon="mdi-check"
+                                                    icon="mdi-delete"
                                                     size="sm"
                                                 ></v-icon>
                                             </v-btn>
                                         </div>
-                                        <div>{{ task.name }}</div>
                                     </div>
-                                </div>
-
-                                <div>
-                                    <v-btn
-                                        size="sm"
-                                        variant="flat"
-                                        color="red"
-                                        class="pa-1"
-                                        @click="deleteTodo(task.id)"
-                                    >
-                                        <v-icon
-                                            icon="mdi-delete"
-                                            size="sm"
-                                        ></v-icon>
-                                    </v-btn>
-                                </div>
-                            </div>
-                        </v-list-item>
-                        <v-list-item
-                            v-if="tasks.length <= 0"
-                            class="todo-item rounded-lg ma-2"
-                        >
-                            <p>No tasks.</p>
-                        </v-list-item>
-                    </v-list>
+                                </v-list-item>
+                            </TransitionGroup>
+                            <v-list-item
+                                v-if="tasks.length <= 0"
+                                class="todo-item rounded-lg ma-2"
+                            >
+                                <p>No tasks.</p>
+                            </v-list-item>
+                        </v-list>
+                    </div>
                 </v-card-text>
 
                 <v-card-actions>
@@ -177,5 +189,41 @@ const deleteAll = (id: number) => toDo.deleteAll();
 }
 .v-field__outline {
     border: 1px solid red;
+}
+
+.task-list {
+    min-height: 100px;
+    max-height: 300px;
+    overflow: auto;
+}
+.task-done {
+    text-decoration-line: line-through;
+    text-decoration-color: #000;
+    transition: text-decoration-line 2s linear;
+}
+
+.list-move,
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.5s linear;
+}
+
+.list-enter-from,
+.list-leave-to {
+    top: 0;
+    opacity: 0;
+    position: absolute;
+}
+
+.list-leave-active {
+    position: absolute;
+}
+
+.list-item-delete {
+    opacity: 0;
+}
+
+.list-item-delete:hover {
+    opacity: 1;
 }
 </style>
